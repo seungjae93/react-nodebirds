3d908f95c1e14fcfa32b73f36955ef098fec7287
seungjae93 <lsj72023892@gmail.com>
2023-02-24 23:25:30 +0900

    :spakrles: feat: antd grid 넣기

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -18,12 +18,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup" legacyBehavior>
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://mydvdiary.tistory.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Made by SeungJae
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
