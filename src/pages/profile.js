fb13161572b2b719b00aa15ba411aa7d662d45d0
seungjae93 <lsj72023892@gmail.com>
2023-02-27 23:47:16 +0900

    :sparkles: feat: profile FollowList 컴포넌트 추가

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "@/components/AppLayout";
+import NicknameEditForm from "@/components/NicknameEditForm";
+import FollowList from "@/components/FollowList";
 
 const profile = () => {
+  //더미데이터
+  const followerList = [
+    { nickname: "seungjae" },
+    { nickname: "SJ" },
+    { nickname: "경통포세이돈" },
+  ];
+  const followingList = [
+    { nickname: "seungjae" },
+    { nickname: "seungjae" },
+    { nickname: "seungjae" },
+  ];
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
