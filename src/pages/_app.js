63966158e81ba5fd2f2c0b1a5592708d3cf14008
seungjae93 <lsj72023892@gmail.com>
2023-03-13 23:08:02 +0900

    :sparkles: feat: store 설정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 
+import wrapper from "@/store/configureStore";
+
 const NodeBird = ({ Component }) => {
   return (
     <>
@@ -18,4 +20,4 @@ NodeBird.prototype = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
